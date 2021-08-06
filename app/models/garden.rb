06db323146f818fef81c30779cef98aa52e904b3
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-06 10:54:16 -0600

    Complete user story 3

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,9 +2,9 @@ class Garden < ApplicationRecord
   has_many :plots
 
   def low_days_to_harvest_plants
-    plots.joins(:plants)
+    wip = plots.joins(:plants)
          .select('DISTINCT plants.*')
-         .where(plants: {days_to_harvest: 90})
+         .where('plants.days_to_harvest < ?', 100)
          .order('plants.id')
   end
 end
